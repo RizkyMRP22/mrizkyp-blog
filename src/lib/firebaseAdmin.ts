ce01c5d58ccb944a30f108025cfb12bfcea61271
telkomdev-mrizkyp22 <mohammad.rizky@digitaltelkom.id>
2026-04-05 09:55:50 +0700

    comment firebaseAdmin

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -1,19 +1,19 @@
-import * as admin from 'firebase-admin';
+// import * as admin from 'firebase-admin';
 
-// Initialize Firebase Admin only once
-if (!admin.apps.length) {
-    try {
-        admin.initializeApp({
-            credential: admin.credential.cert({
-                projectId: process.env.FIREBASE_PROJECT_ID,
-                clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-                // Handle newlines in the private key from env variables
-                privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-            }),
-        });
-    } catch (error) {
-        console.error('Firebase admin initialization error', error);
-    }
-}
+// // Initialize Firebase Admin only once
+// if (!admin.apps.length) {
+//     try {
+//         admin.initializeApp({
+//             credential: admin.credential.cert({
+//                 projectId: process.env.FIREBASE_PROJECT_ID,
+//                 clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+//                 // Handle newlines in the private key from env variables
+//                 privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+//             }),
+//         });
+//     } catch (error) {
+//         console.error('Firebase admin initialization error', error);
+//     }
+// }
 
-export const db = admin.firestore();
+// export const db = admin.firestore();
